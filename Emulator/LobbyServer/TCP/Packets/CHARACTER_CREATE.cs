2c284befa992f4744c86d06c7ae936599e83f0d7
fiki574 <fiki8796@hotmail.com>
2017-01-31 12:33:52 +0100

    Fixed a problem with slot when creating new char

--- Emulator/LobbyServer/TCP/Packets/CHARACTER_CREATE.cs
+++ Emulator/LobbyServer/TCP/Packets/CHARACTER_CREATE.cs
@@ -29,27 +29,16 @@ namespace LobbyServer.TCP.Packets
                 Out.WriteInt32Reverse(cclient.Pending.Slot);
             }
             cclient.Send(Out);
-            System.Threading.Thread.Sleep(200);
+            System.Threading.Thread.Sleep(500);
             cclient.Pending = default(CharacterEntry);
-            System.Threading.Thread.Sleep(200);
+            System.Threading.Thread.Sleep(500);
             cclient.Disconnect();
             return 0;
         }
 
         public byte GetFreeSlot(LobbyClient client)
         {
-            try
-            {
-                byte[] slots = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-                if (client.Characters.Count != 0) for (int i = 0; i < slots.Length; i++) foreach (CharacterEntry ch in client.Characters) if (ch.Slot != slots[i])  return slots[i];
-                else return 1;
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return 0;
-            }
-            return 0;
+            return (byte)(client.Characters.Count + 1);
         }
     }
 }
